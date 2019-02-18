8eaa3036a3f6538c72fc4d461a51e5088b18673e
5habeeb <ahmadshabeeb93@gmail.com>
2019-02-18 23:23:21 +0100

    add calculations to single event driven

--- singleEventDrivenTest.c
+++ singleEventDrivenTest.c
@@ -4,24 +4,6 @@
 #include<time.h>
 #include "singleEventDriven.h"
 
-int simulate_test(){
-    int number_of_enqueued_elements = 3;
-    int max_number_of_elements = 1000;
-    time_t t;
-    srand ((unsigned) time(&t));
-    enqueue((double) time(NULL));
-
-    while(getListSize() > 0 && getListSize() < max_number_of_elements){
-        double timeStamp = dequeue();
-        int j;
-        //printf("dequeued: %lf, enqueued: ", timeStamp);
-        for(j = 0; j < number_of_enqueued_elements; j++){
-            generateEvent(timeStamp);
-        }
-        //printList();
-    }
-}
-
 void enqueue_to_empty_list() {
     time_t t;
     srand ((unsigned) time(&t));
@@ -77,29 +59,105 @@ void generate_random_priority_test() {
     printf("\nExpected %lf - result %lf", 50.5, actualAverage);
 }
 
+
+void populate_list() {
+    int i;
+    double randPriority;
+    srand(time(0));
+    int number_of_iterations = 100000;
+    for (i=0; i< number_of_iterations; i++) {
+        randPriority = increment_randomly();
+        enqueue(randPriority);
+        //printf("%d", randPriority);
+    }
+    //printList();
+}
+
 void clean_list() {
     while (getListSize() > 0) {
         dequeue();
     }
 }
 
-void get_time_of_simulation() {
-    int i;
-    double sum, average;
+void get_time_to_enqueue() {
+    populate_list();
     clock_t start, end;
-    for(i=0; i< 20; i++){
+    srand(time(0));
+    double rand = increment_randomly();
 
+    double sum = 0;
+    double mean = 0;
+    int i;
+    for(i=0; i<100; i++){
+        start = clock();
+        enqueue(rand);
+        end = clock();
+        sum += (end - start) / (double) CLOCKS_PER_SEC;
+        dequeue();
+    }
+    mean = sum / 100;
+    printf("\n\n%lf\n", mean);
+    clean_list();
+}
+
+
+void get_time_to_dequeue() {
+    populate_list();
+    printf("List size: %d\n", getListSize());
+    clock_t start, end;
+    srand(time(0));
+    double rand = increment_randomly();
+
+    double sum = 0;
+    double mean = 0;
+    int i;
+    for(i=0; i<100; i++){
+        start = clock();
+        dequeue();
+        end = clock();
+        sum += (end - start) / (double) CLOCKS_PER_SEC;
+        printf("sum: %lf\n", sum);
+        enqueue(rand);
+    }
+    mean = sum / 100;
+    printf("\n\n%lf\n", mean);
+    clean_list();
+}
+
+
+int simulate_test(){
+    int number_of_enqueued_elements = 3;
+    int max_number_of_elements = 100000;
+    time_t t;
+    srand ((unsigned) time(&t));
+    enqueue((double) time(NULL));
+
+    while(getListSize() > 0 && getListSize() < max_number_of_elements){
+        double timeStamp = dequeue();
+        int j;
+        //printf("dequeued: %lf, enqueued: ", timeStamp);
+        for(j = 0; j < number_of_enqueued_elements; j++){
+            generateEvent(timeStamp);
+        }
+        //printList();
+    }
+}
+
+void get_time_of_simulation() {
+    clock_t start, end;
+    double sum = 0;
+    double mean = 0;
+    int i;
+
+    for(i=0; i<100; i++){
         start = clock();
         simulate_test();
         end = clock();
 
-        double time_to_enqueue = (end - start) / (double) CLOCKS_PER_SEC;
-        sum += time_to_enqueue;
-        clean_list();
+        sum += (end - start) / (double) CLOCKS_PER_SEC;
     }
-    average = sum / 5.0;
-    double time_to_enqueue = (end - start) / (double) CLOCKS_PER_SEC;
-    printf("\n%f\n", average);
+    mean = sum / 100;
+    printf("\n\n%lf\n", mean);
     clean_list();
 }
 
@@ -111,5 +169,7 @@ int main() {
     //dequeue_element_from_list();
     //get_list_size_test();
     //generate_random_priority_test();
+    //get_time_to_enqueue();
+    //get_time_to_dequeue()
     get_time_of_simulation();
 }
